ecd016a6374d6c9e0030953c7a71ae4834a1229d
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-02 22:10:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LiveScorePage from "./pages/LiveScorePage";
 import LiveMatch from "./pages/LiveMatch";
+import LiveTrainPage from ".pages/TrainTracking";
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
 
         <Route path="/live-score/:matchId" element={<LiveScorePage />}/>
         <Route path="/live-match/:matchId" element={<LiveMatch />}/>
+        <Route path="/train-tracking/:trainNo" element={<LiveMatch />}/>
         <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
