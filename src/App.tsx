263da3a05119e4159a542d6ed2a70a14d352faca
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-02 17:28:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import HomePage from "./pages/HomePage";
+import LiveScorePage from "./pages/LiveScorePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/live-score/:matchId"
+          element={<LiveScorePage />}
+        />
+
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
