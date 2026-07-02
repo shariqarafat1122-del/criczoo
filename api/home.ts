0beb6897a2763ddf55d40c63962c0eb041ed6994
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-02 17:02:34 +0530

    Add files via upload

--- api/home.ts
+++ api/home.ts
@@ -1,1 +1,12 @@
+export default async function handler(req: any, res: any) {
+  const response = await fetch("https://www.cricbuzz.com/api/home", {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      Accept: "application/json",
+    },
+  });
 
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
