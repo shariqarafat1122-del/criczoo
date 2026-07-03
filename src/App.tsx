9ee443d1a05344970922fb98aed29f702a2f2cf7
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-03 14:38:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LiveScorePage from "./pages/LiveScorePage";
 import LiveMatch from "./pages/LiveMatch";
 import LiveTrainPage from "./pages/TrainTracking";
 import Squad from './pages/Squad';
+import PlayerProfilePage from './pages/Profile';
 
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/live-match/:matchId" element={<LiveMatch />}/>
         <Route path="/train-tracking" element={<LiveTrainPage />}/>
         <Route path="/team-squad/:matchId" element={<Squad />}/>
+        <Route path="/profile/:profileId" element={<Squad />}/>
         <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
