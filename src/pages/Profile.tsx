f02f8323a75f410369dc36837fee81cb68a60e1a
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-04 20:21:02 +0530

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -734,7 +734,7 @@ export default function PlayerProfilePage() {
     if (!profileId) return;
     setStatus("loading");
     try {
-      const res = await fetch(`/api/player/profile?profileId=${profileId}`);
+      const res = await fetch(/api/player/profile?profileId="9443/tim-seifert");
       if (res.status === 404) {
         setStatus("notfound");
         return;
