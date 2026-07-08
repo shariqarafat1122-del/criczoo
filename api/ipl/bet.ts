667cf0470dba2dc182c539ec74aa0359d7632486
shariqarafat1122-del <shariqarafat1122@gmail.com>
2026-07-08 10:16:22 +0530

    Update bet.ts

--- api/ipl/bet.ts
+++ api/ipl/bet.ts
@@ -1,22 +1,12 @@
 export default async function handler(req: any, res: any) {
-  try {
+  const response = await fetch("https://betadda.vercel.app/api/poker/create", {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      Accept: "application/json",
+    },
+  });
 
-    const response = await fetch(
-      "https://betadda.vercel.app/api/poker/create"),
-      {
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Accept": "application/json",
-        },
-      }
-    );
+  const data = await response.json();
 
-    const text = await response.text();
-
-  return res.status(200).send(text);
-  } catch (error: any) {
-    return res.status(500).json({
-      error: error.message,
-    });
-  }
+  res.status(200).json(data);
 }
